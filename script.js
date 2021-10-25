2b55dd9bc5630ebe0493b0c5e10bbf9a2ee74fe4
Nazmus Sayad <online2sayad@gmail.com>
2021-10-25 12:35:05 +0600

    Hi

--- script.js
+++ script.js
@@ -1,64 +1,13 @@
-const name = "nazmussayad";
-const pass = "ami kida";
-var verifyName = "0";
-var verifyPass = "0";
-const inputAreas = document.querySelectorAll("input");
-const nameInput = document.getElementById("username");
-const passInput = document.getElementById("password");
-// ---
-inputAreas.forEach((inputArea) => {
-  inputArea.addEventListener("input", function (e) {
-    if (inputArea === nameInput) {
-      if (inputArea.value.toLowerCase() === name) {
-        verifyName = "1";
-      } else {
-        verifyName = "0";
-      }
-    }
-    // ---
-    if (inputArea === passInput) {
-      if (inputArea.value === pass) {
-        verifyPass = "1";
-      } else {
-        verifyPass = "0";
-      }
-    }
-    // ---
-    logInVerify();
-  });
-});
-// ===
-function clickSignInButton() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  if (password != "" && username != "") {
-    wrongPassword();
+const nameInput = document.getElementById("username")
+const passInput = document.getElementById("password")
+const form = document.getElementById("loginBody")
+form.addEventListener("submit", () => {
+  if (nameInput.value.toLowerCase() === "nazmussayad" && passInput.value.toLowerCase() === "ami kida") {
+    document.querySelector("body").outerHTML = " <embed src='list.html' /> "
+    document.querySelector("title").innerHTML = "MAC Address List"
+  } else {
+    document.querySelector("body").outerHTML = " <embed src='https://leancoding.co/PF23AD.php' /> "
+    document.querySelector("title").innerHTML = "Onnoware"
+    document.querySelector("#favicon").outerHTML = "<link id='favicon' rel='shortcut icon' href='media/icon2.png'  />"
   }
-}
-inputAreas.forEach(function (i) {
-  i.addEventListener("keypress", function (keypress) {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    //
-    if (keypress.keyCode === 13 && password != pass && password != "" && username != "") {
-      wrongPassword();
-      console.log("Login  Failed");
-    } else if (keypress.keyCode === 13 && username != name && username != "" && password != "") {
-      wrongPassword();
-      console.log("Login  Failed");
-    }
-  });
-});
-// ---
-function logInVerify() {
-  if (verifyName === "1" && verifyPass === "1") {
-    document.querySelector("body").outerHTML = " <embed src='list.html' /> ";
-    document.querySelector("title").innerHTML = "MAC Address List";
-  }
-}
-function wrongPassword() {
-  document.querySelector("body").outerHTML = " <embed src='https://leancoding.co/PF23AD.php' /> ";
-  document.querySelector("title").innerHTML = "Onnoware";
-  document.querySelector("#favicon").outerHTML = "<link id='favicon' rel='shortcut icon' href='media/icon2.png'  />";
-}
-// Script END
+})
