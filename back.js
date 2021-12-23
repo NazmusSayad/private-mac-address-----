ca35343210e35e4a5b7ca2697e5f37cd021723da
Nazmus Sayad <online2sayad@gmail.com>
2021-12-23 11:35:28 +0600

    Update

--- back.js
+++ back.js
@@ -27,37 +27,48 @@ const verifyLog = (username, password) => {
 function epd() {
   event.preventDefault();
 }
-document.addEventListener("copy", epd);
-document.addEventListener("contextmenu", epd);
-document.addEventListener("dragstart", epd);
-document.addEventListener("selectstart", epd);
-document.addEventListener("cut", epd);
-document.addEventListener("touchstart", epd);
-document.addEventListener("paste", epd);
-document.addEventListener("keydown", epd);
-
+// ------------------
+const search = function () {
+  const elms = [
+    ...document.querySelectorAll("#userItem article"),
+    ...document.querySelectorAll("#otherItem article"),
+    ...document.querySelectorAll("#unItem article"),
+  ];
+  elms.forEach((element) => {
+    if (!element.textContent.toLowerCase().includes(input.value.toLowerCase())) {
+      element.style.display = "none";
+    } else {
+      element.removeAttribute("Style");
+    }
+  });
+};
 // ------------------
 const execList = () => {
   document.title = "MAC Addresses ";
   const elMain = document.createElement("main");
   elMain.setAttribute("id", `listPage`);
   elMain.innerHTML = `
-  <section class="mainItemCon" id="ownerItem">
-    <p class="heading">Omnitrix</p>
-    <section class="con"></section>
-  </section>
-  <section class="mainItemCon" id="userItem">
-    <p class="heading">Users</p>
-    <section class="con"></section>
-  </section>
-  <section class="mainItemCon" id="otherItem">
-    <p class="heading">Others</p>
-    <section class="con"></section>
-  </section>
-  <section class="mainItemCon" id="unItem">
-    <p class="heading">Unknown</p>
-    <section class="con"></section>
-  </section>`;
+<section class="mainItemCon" id="ownerItem">
+  <p class="heading">Omnitrix</p>
+  <section class="con"></section>
+</section>
+<section class="search">
+  <i class="searchIcon ico" onclick="this.parentNode.querySelector('input').focus()" ><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></i>
+  <input autocomplete="off" type="text">
+  <i class="crossIcon ico" onclick="this.parentNode.querySelector('input').value='';this.parentNode.querySelector('input').focus();" ><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg></i>
+</section>
+<section class="mainItemCon" id="userItem">
+  <p class="heading">Users</p>
+  <section class="con"></section>
+</section>
+<section class="mainItemCon" id="otherItem">
+  <p class="heading">Others</p>
+  <section class="con"></section>
+</section>
+<section class="mainItemCon" id="unItem">
+  <p class="heading">Unknown</p>
+  <section class="con"></section>
+</section>`;
   document.querySelector("body").prepend(elMain);
   const ownerItem = document.querySelector("#ownerItem > .con");
   const userItem = document.querySelector("#userItem > .con");
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+document.addEventListener("copy", epd);
+document.addEventListener("contextmenu", epd);
+document.addEventListener("dragstart", epd);
+document.addEventListener("selectstart", epd);
+document.addEventListener("cut", epd);
+document.addEventListener("touchstart", epd);
+document.addEventListener("paste", epd);
+// ------------------
 if (!macStatus) {
   execLogin();
 } else if (macStatus.u && macStatus.p && verifyLog(macStatus.u, macStatus.p)) {
   execList();
 }
 // ------------------
+const input = document.querySelector(".search input");
+input.addEventListener("input", search);
+document.querySelectorAll(".search i").forEach((e) => {
+  e.addEventListener("click", search);
+});
