06645d55b72bf93f63fb0c8e861ab763d9f87f92
Nazmus Sayad <online2sayad@gmail.com>
2022-01-21 10:56:50 +0600

    Update...

---
+++ src/script/script.js
@@ -0,0 +1,10 @@
+const macStatus = (() => {
+  return verifyLog(nCookie("u"), nCookie("p"))
+})()
+// ------------------
+let contextArticle
+document.addEventListener("contextmenu", epd)
+document.addEventListener("dragstart", epd)
+// ------------------
+macStatus ? execList() : execLogin()
+// ------------------
